8d2619d06add80ae2cdb08f0bffc241d0bbf5296
berkingurcan <berkingurcan@yandex.com>
2023-04-15 22:28:37 +0900

    Update Deploy.s.sol

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -10,11 +10,11 @@ contract DeployZKDrop is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         address _safe = 0x0911BA4aE8D1cbC39A3f42bC9F91B06c0d681609;
-        bytes16 _appId = 0x11627eb6dd3358f8f4434a94bd15e6c5;
-        bytes16 _groupId = 0x8837536887a7f6458977b10cc464df4b;
+        bytes16 _appId = 0x282ff775a754ebea339746f096635a5a;
+        bytes16 _groupId = 0x84f8495423ea1aa1b212356f31f6c7d9;
 
-        new SismoConnectModule(_safe, _appId, _groupId);
-
+        SismoConnectModule sismoConnectModule = new SismoConnectModule(_safe, _appId, _groupId);
+        console.log("sismoconnectmodule delpoyed to", address(sismoConnectModule));
         vm.stopBroadcast();
     }
 }
